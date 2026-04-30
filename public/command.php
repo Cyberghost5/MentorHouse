681afb740980c4ff7bd32ffae52f9f061d773e16
Adebisi Covenant <adebisicovenant01@gmail.com>
2026-04-30 16:35:41 +0100

    Added SEO

--- public/command.php
+++ public/command.php
@@ -16,9 +16,9 @@ $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 
-$status = $kernel->call('optimize:clear');
+$status = $kernel->call('migrate');
 
 echo '<pre>';
-echo "optimize:clear exit code: {$status}\n";
+echo "migrate exit code: {$status}\n";
 echo $kernel->output();
 echo '</pre>';
